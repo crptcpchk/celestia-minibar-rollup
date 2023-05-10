8794a5c7a59e88102236c483ae748feef08f3155
crptcpchk <94843482+crptcpchk@users.noreply.github.com>
2023-05-10 14:42:09 +0300

    Update init.sh

--- docs/scripts/recipes/init.sh
+++ docs/scripts/recipes/init.sh
@@ -13,11 +13,11 @@ DA_BLOCK_HEIGHT=$(curl https://rpc-blockspacerace.pops.one/block | jq -r '.resul
 echo $DA_BLOCK_HEIGHT
 
 ignite chain build
-recipesd tendermint unsafe-reset-all
-recipesd init $VALIDATOR_NAME --chain-id $CHAIN_ID
+minibard tendermint unsafe-reset-all
+minibard init $VALIDATOR_NAME --chain-id $CHAIN_ID
 
-recipesd keys add $KEY_NAME --keyring-backend test
-recipesd add-genesis-account $KEY_NAME $TOKEN_AMOUNT --keyring-backend test
-recipesd gentx $KEY_NAME $STAKING_AMOUNT --chain-id $CHAIN_ID --keyring-backend test
-recipesd collect-gentxs
-recipesd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT
+minibard keys add $KEY_NAME --keyring-backend test
+minibard add-genesis-account $KEY_NAME $TOKEN_AMOUNT --keyring-backend test
+minibard gentx $KEY_NAME $STAKING_AMOUNT --chain-id $CHAIN_ID --keyring-backend test
+minibard collect-gentxs
+minibard start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT
